a0a4b6b3c2ec4b73cf0238484f8c0180e7267019
dhanisetiaji <dhanisetiaji99@gmail.com>
2019-08-12 21:37:31 +0700

    upload all

---
+++ bin/functionapiamikom.js
@@ -0,0 +1,169 @@
+const fetch = require ('node-fetch');
+const readlineSync = require ('readline-sync');
+const { URLSearchParams } = require ('url');
+
+
+const functionapiLoginAmikom = (usernim, keynim) => new Promise((resolve, reject) => {
+    const urlapi = 'http://mhsmobile.amikom.ac.id/login';
+    const params = new URLSearchParams;
+    params.append('username', usernim);
+    params.append('keyword', keynim);
+
+    fetch(urlapi, {
+        method: 'POST',
+        headers: {
+            'User-Agent': 'AmikomMobile',
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': '33',
+            'Host': 'mhsmobile.amikom.ac.id',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip'
+        },
+        body: params
+
+    })
+    .then( res => res.json())
+    .then( result => resolve(result))
+    .catch(err => reject(err))
+});
+
+const functionapiJadwal = (tokenauth, npmjadwal, semesterjadwal, tahunjadwal) => new Promise((resolve, reject) => {
+    const urlapi = 'http://mhsmobile.amikom.ac.id/api/personal/jadwal_kuliah';
+    const params = new URLSearchParams;
+    params.append('npm', npmjadwal);
+    params.append('semester', semesterjadwal);
+    params.append('tahun_akademik', tahunjadwal)
+
+    fetch(urlapi, {
+        method: 'POST',
+        headers: {
+            'User-Agent': 'AmikomMobile',
+            Authorization : tokenauth,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': '52',
+            'Host': 'mhsmobile.amikom.ac.id',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip'
+        },
+        body: params
+
+    })
+    .then( res => res.json())
+    .then( result => resolve(result))
+    .catch(err => reject(err))
+});
+
+const functionAbsensi = (codeabsen, nimabsen) => new Promise((resolve, reject) => {
+    const urlabsensi = 'http://kantin.amikom.ac.id/one/presensi_mobile/validate_ticket';
+    const bodyabsensi = {"data":`${codeabsen};${nimabsen}`}
+
+    fetch(urlabsensi, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8',
+            'Content-Length': '28',
+            'Host': 'kantin.amikom.ac.id',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip',
+            'User-Agent': 'okhttp/3.10.0'             
+        },
+        body: JSON.stringify(bodyabsensi)
+    })
+    .then( res => res.json())
+    .then( result => resolve(result))
+    .catch(err => reject(err))
+});
+
+const functionapikhs = (tokenauth, npmkhs, semesterkhs, tahunkhs) => new Promise((resolve, reject) => {
+    const urlapi = 'http://mhsmobile.amikom.ac.id/api/krs/khs';
+    const params = new URLSearchParams;
+    params.append('npm', npmkhs);
+    params.append('semester', semesterkhs);
+    params.append('tahun_akademik', tahunkhs)
+
+    fetch(urlapi, {
+        method: 'POST',
+        headers: {
+            'User-Agent': 'AmikomMobile',
+            Authorization : tokenauth,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': '52',
+            'Host': 'mhsmobile.amikom.ac.id',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip'
+        },
+        body: params
+
+    })
+    .then( res => res.json())
+    .then( result => resolve(result))
+    .catch(err => reject(err))
+});
+
+const functiontranskripnilai = (tokenauth, npmtrans) => new Promise((resolve, reject) => {
+    const urlapi = 'http://mhsmobile.amikom.ac.id/api/krs/transkrip';
+    const params = new URLSearchParams;
+    params.append('npm', npmtrans),
+
+    fetch(urlapi, {
+        method: 'POST',
+        headers: {
+            'User-Agent': 'AmikomMobile',
+            Authorization : tokenauth,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': '14',
+            'Host': 'mhsmobile.amikom.ac.id',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip'
+        },
+        body: params
+
+    })
+    .then( res => res.json())
+    .then( result => resolve(result))
+    .catch(err => reject(err))
+});
+
+const functionBiodata = (tokenauth) => new Promise((resolve, reject) => {
+    const urlapi = 'http://mhsmobile.amikom.ac.id/api/personal/init_data_mhs';
+
+    fetch(urlapi, {
+        method: 'POST',
+        headers: {
+            'User-Agent': 'AmikomMobile',
+            Authorization : tokenauth,
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length': '14',
+            'Host': 'mhsmobile.amikom.ac.id',
+            'Connection': 'Keep-Alive',
+            'Accept-Encoding': 'gzip'
+        }
+
+    })
+    .then( res => res.json())
+    .then( result => resolve(result))
+    .catch(err => reject(err))
+});
+
+
+module.exports = { 
+    functionapiLoginAmikom,
+    functionBiodata,
+    functionAbsensi,
+    functionapiJadwal,
+    functiontranskripnilai,
+    functionapikhs
+}
+
+// (async () => {
+//     try{
+//         const userN = readlineSync.question('Masukan Nim : ');
+//         const keyN = readlineSync.question('Masukan Password : ')
+//         const loginapi = await  functionapiLoginAmikom(userN, keyN);
+//         const logintoken = loginapi.access_token;
+//         const nimlogin = loginapi.userName;
+//         console.log(loginapi);
+//     }catch(e){
+//         console.log(e)
+//     }
+// })();
---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const LoginUAY = require('./bin/functionapiamikom');
+
+const app = express();
+
+app.get('/', (req, res) => {
+    const login="<h1>get list:</h1>/getlogin?usernim=&keynim<br>/getbio?tokenauth=<br>/absen?code=&nim=";
+    res.send(login);
+});
+
+app.get('/getlogin', async (req, res) => {
+    const { usernim, keynim } = req.query;
+    const sendlogin = await LoginUAY.functionapiLoginAmikom(usernim, keynim);
+    res.send(sendlogin);
+});
+
+app.get('/getbio', async (req, res) => {
+    const { tokenauth } = req.query;
+    const sendbio = await LoginUAY.functionBiodata(tokenauth);
+    res.send(sendbio);
+});
+
+app.get('/absen', async (req, res) => {
+    const { code, nim } = req.query;
+    const absenku = await LoginUAY.functionAbsensi(code, nim);
+    res.send(absenku);
+});
+
+app.get('/jadwal', async (req, res) => {
+    const { tokenauth, nim, semsterj, tahunj } = req.query;
+    const jadwalcek = await LoginUAY.functionapiJadwal(tokenauth, nim, semsterj, tahunj);
+    res.send(jadwalcek);
+});
+
+app.listen(3000);
