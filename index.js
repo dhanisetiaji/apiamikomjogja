9c3c9dfe60e909b05a01f8b3b0fb7608ab783b8a
dhanisetiaji <dhanisetiaji99@gmail.com>
2019-08-13 18:52:53 +0700

    ubah get ke post

--- index.js
+++ index.js
@@ -10,37 +10,37 @@ app.get('/', (req, res) => {
     res.send(login);
 });
 
-app.get('/getlogin', async (req, res) => {
+app.post('/getlogin', async (req, res) => {
     const { usernim, keynim } = req.query;
     const sendlogin = await LoginUAY.functionapiLoginAmikom(usernim, keynim);
     res.send(sendlogin);
 });
 
-app.get('/getbio', async (req, res) => {
+app.post('/getbio', async (req, res) => {
     const { tokenauth } = req.query;
     const sendbio = await LoginUAY.functionBiodata(tokenauth);
     res.send(sendbio);
 });
 
-app.get('/absen', async (req, res) => {
+app.post('/absen', async (req, res) => {
     const { code, nim } = req.query;
     const absenku = await LoginUAY.functionAbsensi(code, nim);
     res.send(absenku);
 });
 
-app.get('/jadwal', async (req, res) => {
+app.post('/jadwal', async (req, res) => {
     const { tokenauth, nim, semsterj, tahunj } = req.query;
     const jadwalcek = await LoginUAY.functionapiJadwal(tokenauth, nim, semsterj, tahunj );
     res.send(jadwalcek);
 });
 
-app.get('/transkripnilai', async (req, res) => {
+app.post('/transkripnilai', async (req, res) => {
     const { tokenauth, nim} = req.query;
     const transkrip = await LoginUAY.functiontranskripnilai(tokenauth, nim);
     res.send(transkrip);
 });
 
-app.get('/khs', async (req, res) => {
+app.post('/khs', async (req, res) => {
     const {tokenauth, nim, semsterk, tahunk} = req.query;
     const khsku = await LoginUAY.functiontranskripnilai(tokenauth, nim, semsterk, tahunk);
     res.send(khsku);
